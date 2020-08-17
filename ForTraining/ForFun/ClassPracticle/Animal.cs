5430813cdabcc2205a80b6be076f9516f9833411
Jlovenpk1 <jlovenpk1@mail.ru>
2020-08-18 01:02:41 +0400

    ++

---
+++ ForTraining/ForFun/ClassPracticle/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTraining.ForFun
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public abstract void Roar();
+        public abstract void GetInfo();
+        public abstract void Eat();
+        public abstract void Run();
+        public abstract void Walk();
+    }
+}
---
+++ ForTraining/ForFun/ClassPracticle/MakeAnimal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTraining.ForFun.ClassPracticle
+{
+    class MakeAnimal
+    {
+        public void Run()
+        {
+
+        }
+    }
+
+    class Cat : Animal
+    {
+        private string _name;
+        public override string Name { get => _name; set => _name = value }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Nam Nam");
+        }
+
+        public override void GetInfo()
+        {
+            Console.WriteLine("Name: {0} ", Name);
+        }
+
+        public override void Roar()
+        {
+            Console.WriteLine("Nya-Nya");
+        }
+
+        public override void Run()
+        {
+            Console.WriteLine("{0} Run", Name);
+        }
+
+        public override void Walk()
+        {
+            Console.WriteLine("{0} Walk", Name);
+        }
+    }
+}
